bf19a0a6718f5007b9e52fb573146b461915ba4d
Barrnet <barrnet@videogamezone.eu>
2022-03-23 23:22:32 +0100

    Elimitato una modifica presente per errore

--- public/inc/register.php
+++ public/inc/register.php
@@ -13,7 +13,6 @@ if (!defined('IN_SCRIPT'))
 		<input name="email-confirm" type="text" placeholder="<?php echo REGISTER_LABEL_PLACEHOLDER_MAIL_CONFIRM;?>" />
 		<input name="password" type="password" placeholder="<?php echo REGISTER_LABEL_PLACEHOLDER_PSW;?>" />
 		<input name="password-confirm" type="password" placeholder="<?php echo REGISTER_LABEL_PLACEHOLDER_PSW_CONFIRM;?>" />
-		<input name="disagio_pw" type="password" placeholder="Password di sblocco" />
 		<input name="submit" type="submit" value="<?php echo REGISTER_LABEL_REGISTER_BUTTON;?>" />
 	</form>
 </div>
